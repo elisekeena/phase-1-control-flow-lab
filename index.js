33e4093cde2788b5c3e4a62e396cc553bc38f46f
Elise <elise041294@gmail.com>
2022-04-04 20:53:41 -0600

    commit tree bad33fd17536a9982da14dea85aa3c2efb8edd48

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+// function scuberGreetingForFeet(){
   // Write your code here!
+  function scuberGreetingForFeet(someValue){
+    let result
+    if(someValue <= 400){
+      result = "This one is on me!"
+      return result
+    }
+    else if (someValue >400 && someValue< 2000){
+      result = "That will be twenty bucks."
+      return result
+    }  
+    else if (someValue >=2000 && someValue<= 2500){
+      result = "I will gladly take your thirty bucks."
+      return result
+    }
+    else {
+      result = "No can do."
+      return result
+    }
+  };
+
+function ternaryCheckCity(city){
+  return (city == "NYC") ? "Ok, sounds good." : "No go."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  // Write your code here
+  let result
+  switch (tip){
+    case "generous":
+      result = "Thank you so much."
+      return result
+    case "not as generous":
+      result = "Thank you."
+      return result
+    default:
+      result= "Bye."
+      return result
+  }
+};
